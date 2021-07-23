e5ada2c046e0c0f70b134f6c409d13e7782b90cc
JuliusDeng <1576554007@qq.com>
2021-07-23 19:13:42 +0800

    顶部导航栏开发 rpx不可过度依赖

--- App.vue
+++ App.vue
@@ -2,6 +2,14 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			// #ifdef APP-NVUE
+			// 加载公共图标库
+			const domModule = weex.requireModule('dom')
+			domModule.addRule('fontFace', {
+			    'fontFamily': "iconfont",
+			    'src': "url('https://at.alicdn.com/t/font_1365296_vumpzwvr0z.ttf')"
+			});
+			// #endif
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -15,6 +23,10 @@
 <style>
 	/*每个页面公共css */
 	@import url("@/common/free.css");
-	
+	@import "./common/common.css";
+	/* #ifndef APP-PLUS-NVUE */
+	/* 加载公共图标库 */
+	@import "./common/free-icon.css";
+	/* #endif */
 	
 </style>
