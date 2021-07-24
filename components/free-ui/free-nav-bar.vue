dc25feeb9060318d34e0d123c5fd769e9b475ab3
JuliusDeng <1576554007@qq.com>
2021-07-24 19:03:59 +0800

    优化图标显示问题。

---
+++ components/free-ui/free-nav-bar.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<view class="bg-light" :class="fixed?'fixed-top':''">
+			<!-- 状态栏 -->
+			<view :style="'height:'+statusBarHeight+'px'"></view>
+			<!-- 导航 -->
+			<view class="w-100 flex align-center justify-between" style="height: 90rpx;">
+				<!-- 左边 -->
+				<view class="flex align-center">
+					<!-- 标题 -->
+					<text v-if="title" 
+					class="font-md ml-3"
+					>{{getTitle}}</text>
+				</view>
+				<!-- 右边 -->
+				<view class="flex align-center">
+					<free-icon-button :icon="'\ue6e3'"></free-icon-button>
+					<free-icon-button :icon="'\ue682'"></free-icon-button>
+				</view>
+			</view>
+		</view>
+		<!-- 占位 -->
+		<view v-if="fixed" :style="fixedStyle"></view>
+	</view>
+</template>
+
+<script>
+	import freeIconButton from "./free-icon-button.vue"
+	export default {
+		props: {
+			title: {
+				type: [String,Boolean],
+				default:false 
+			},
+			fixed:{
+				type:Boolean,
+				default:true
+			},
+			noreadnum: {
+				type: Number,
+				default: 0
+			}
+		},
+		components:{
+			freeIconButton
+		},
+		data() {
+			return {
+				statusBarHeight:0,
+				navBarHeight:0
+			}
+		},
+		mounted() {
+			// #ifdef APP-PLUS-NVUE
+			this.statusBarHeight = plus.navigator.getStatusbarHeight()
+			// #endif
+			this.navBarHeight = this.statusBarHeight + uni.upx2px(90)
+		},
+		computed: {
+			fixedStyle() {
+				return `height:${this.navBarHeight}px`
+			},
+			getTitle() {
+				let noreadnum = this.noreadnum > 0 ? '('+this.noreadnum+')' : '' 
+				return this.title + noreadnum
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
--- note-01.md
+++ note-01.md
@@ -1,6 +1,9 @@
 # weex/nvue  笔记
 
-# 1 view里面的style不支持``模板字符串写法  如果：view :style=`'height':${statusBarHeight}'px'`></view>
+# 1 view里面的style不支持``模板字符串写法  
+# eg：view :style=`'height':${statusBarHeight}'px'`></view> // 不生效，未知bug
+# computed中当然可以写``模板字符串写法
 
 # 2 [官方已说不要太依赖rpx 如高度和字体是不应该根据屏幕宽度（等比）变化的](https://ask.dcloud.net.cn/article/36130)顺便提醒另一个事情，很多开发者对响应式单位依赖太严重了，比如组件高度或字体大小也使用upx/rpx。 注意只有当你需要某元素的单位要根据屏幕宽度（小范围）大小变化时，才需要rpx这类动态宽度单位。 一般情况下高度和字体大小是不应该根据屏幕宽度（等比）变化的。
 
+# 3 注：<slot></slot>组件默认在外层家里<text></text>,实际是如此：<text><slot></slot></text>  而<text>组件不可内嵌自身。
